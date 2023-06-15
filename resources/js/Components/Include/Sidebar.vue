e5045a997c02be1aa1fa7819cc73ab070f6aad7c
asif khan <asiff080@gmail.com>
2023-06-15 11:40:49 +0500

    change in sidebar in layout and create payment index page

--- resources/js/Components/Include/Sidebar.vue
+++ resources/js/Components/Include/Sidebar.vue
@@ -2,30 +2,45 @@
     import {Link} from "@inertiajs/vue3";
 </script>
 <template>
-    <div class="nk-sidebar-element">
-        <div class="nk-sidebar-content">
-            <div class="nk-sidebar-menu" data-simplebar>
-                <ul class="nk-menu">
-                    <li class="nk-menu-heading">
-                        <h6 class="overline-title text-primary-alt">Use-Case Preview</h6>
-                    </li>
-                    <li class="nk-menu-item">
-                        <Link :href="route('dashboard')" class="nk-menu-link">
-                            <span class="nk-menu-icon"><em class="icon ni ni-dashboard"></em></span>
-                            <span class="nk-menu-text">Dashboard</span>
-                        </Link>
-                    </li>
-                    <li class="nk-menu-item">
-                        <Link :href="route('users')" class="nk-menu-link">
-                            <span class="nk-menu-icon"><em class="icon ni ni-dashboard"></em></span>
-                            <span class="nk-menu-text">Users</span>
-                        </Link>
-                    </li>
+    <div class="nk-sidebar nk-sidebar-fixed is-light " data-content="sidebarMenu">
+        <div class="nk-sidebar-element nk-sidebar-head">
+            <div class="nk-sidebar-brand">
+                <a href="html/index.html" class="logo-link nk-sidebar-logo">
+                    <img class="logo-light logo-img" src="assets/images/logo.png" srcset="assets/images/logo2x.png 2x" alt="logo">
+                    <img class="logo-dark logo-img" src="assets/images/logo-dark.png" srcset="assets/images/logo-dark2x.png 2x" alt="logo-dark">
+                    <img class="logo-small logo-img logo-img-small" src="assets/images/logo-small.png" srcset="assets/images/logo-small2x.png 2x" alt="logo-small">
+                </a>
+            </div>
+            <div class="nk-menu-trigger mr-n2">
+                <a href="#" class="nk-nav-toggle nk-quick-nav-icon d-xl-none" data-target="sidebarMenu"><em class="icon ni ni-arrow-left"></em></a>
+                <a href="#" class="nk-nav-compact nk-quick-nav-icon d-none d-xl-inline-flex" data-target="sidebarMenu"><em class="icon ni ni-menu"></em></a>
+            </div>
+        </div><!-- .nk-sidebar-element -->
+        <div class="nk-sidebar-element">
+            <div class="nk-sidebar-content">
+                <div class="nk-sidebar-menu" data-simplebar>
+                    <ul class="nk-menu">
+                        <li class="nk-menu-heading">
+                            <h6 class="overline-title text-primary-alt">Use-Case Preview</h6>
+                        </li>
+                        <li class="nk-menu-item">
+                            <Link :href="route('dashboard')" class="nk-menu-link">
+                                <span class="nk-menu-icon"><em class="icon ni ni-dashboard"></em></span>
+                                <span class="nk-menu-text">Dashboard</span>
+                            </Link>
+                        </li>
+                        <li class="nk-menu-item">
+                            <Link :href="route('users')" class="nk-menu-link">
+                                <span class="nk-menu-icon"><em class="icon ni ni-dashboard"></em></span>
+                                <span class="nk-menu-text">Users</span>
+                            </Link>
+                        </li>
 
 
 
-                </ul><!-- .nk-menu -->
-            </div><!-- .nk-sidebar-menu -->
-        </div><!-- .nk-sidebar-content -->
+                    </ul><!-- .nk-menu -->
+                </div><!-- .nk-sidebar-menu -->
+            </div><!-- .nk-sidebar-content -->
+        </div>
     </div>
 </template>
