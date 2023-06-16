c53a01bc79c9c73f7fb29bffc0587ac42e524cef
asif khan <asiff080@gmail.com>
2023-06-16 10:42:26 +0500

    install breez with blade directives

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,72 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Inertia\Inertia;
-use Inertia\Response;
-use App\Models\UserProfile;
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Redirect;
-use App\Http\Requests\ProfileUpdateRequest;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-    public function index()
-    {
-        return Inertia::render('Auth/User-profile');
-    }
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): Response
+    public function edit(Request $request): View
     {
-        return Inertia::render('Profile/Edit', [
-            'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
-            'status' => session('status'),
+        return view('profile.edit', [
+            'user' => $request->user(),
         ]);
     }
 
     /**
      * Update the user's profile information.
      */
-    // public function update(ProfileUpdateRequest $request): RedirectResponse
-    // {
-    //     $request->user()->fill($request->validated());
-
-    //     if ($request->user()->isDirty('email')) {
-    //         $request->user()->email_verified_at = null;
-    //     }
-
-    //     $request->user()->save();
-
-    //     return Redirect::route('profile.edit');
-    // }
-
-    public function update(Request $request)
+    public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->validate([
-            'dob' => 'required|date',
-            'country' => 'required',
-            'city' => 'required',
-            'address' => 'required',
-            'postal_code' => 'required',
-            'country_code' => 'required',
-            'phone_no' => 'required',
-        ]);
+        $request->user()->fill($request->validated());
 
-        UserProfile::where('user_id', Auth::id())->update([
-            'date_of_birth' => $request->dob,
-            'country' => $request->country,
-            'city' => $request->city,
-            'address' => $request->address,
-            'postal_code' => $request->postal_code,
-            'country_code' => $request->country_code,
-            'phone_number' => $request->phone_no,
-        ]);
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
 
     /**
@@ -75,7 +42,7 @@ class ProfileController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
-        $request->validate([
+        $request->validateWithBag('userDeletion', [
             'password' => ['required', 'current_password'],
         ]);
 
@@ -89,53 +56,5 @@ class ProfileController extends Controller
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'dob' => 'required|date',
-            'country' => 'required',
-            'city' => 'required',
-            'address' => 'required',
-            'postal_code' => 'required',
-            'country_code' => 'required',
-            'phone_no' => 'required',
-        ]);
-
-        try {
-            $path = '';
-            if ($request->hasFile('avatar')) {
-                $request->validate([
-                    'image' => 'mimes:png,jpg,jpeg,gif'
-                ]);
-                $path = $request->file('avatar')->store('User/Images', 'public');
-            }
-            UserProfile::create([
-                'user_id' => Auth::id(),
-                'date_of_birth' => $request->dob,
-                'country' => $request->country,
-                'city' => $request->city,
-                'address' => $request->address,
-                'postal_code' => $request->postal_code,
-                'country_code' => $request->country_code,
-                'phone_number' => $request->phone_no,
-                'avatar' => $request->path,
-            ]);
-            return redirect()->route('user.profile.show');
-        } catch (\Throwable $th) {
-            dd($th->getMessage());
-            return back();
-        }
-    }
-
-    public function show()
-    {
-        $user = User::with('user_profile')->where('id', Auth::id())->first();
-        if (!$user->user_profile)
-            return redirect()->route('user.profile');
-        return Inertia::render('User', [
-            'user' => $user
-        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use Inertia\Inertia;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Admin\DepositMethodController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,32 +9,23 @@ use App\Http\Controllers\Admin\DepositMethodController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-
 Route::get('/', function () {
-    return redirect()->route('login');
+    return view('welcome');
 });
 
-Route::group(['middleware' => ['auth', 'role:Admin'], 'prefix' => 'admin'], function () {
-
-    Route::resource('/deposit_method', DepositMethodController::class);
-});
-
-
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-    Route::get('/user/profile', [ProfileController::class, 'index'])->name('user.profile');
-    Route::get('/user/profile/show', [ProfileController::class, 'show'])->name('user.profile.show');
-    Route::post('/user/profile/update', [ProfileController::class, 'update'])->name('user.profile.update');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::post('/profile/store', [ProfileController::class, 'store'])->name('profile.store');
 });
 
+require __DIR__.'/auth.php';
