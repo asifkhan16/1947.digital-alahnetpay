86db99a86d06c22b947cfdc556348fb71d5e7ac2
asif khan <asiff080@gmail.com>
2023-06-16 14:21:07 +0500

    create update and destroy function

--- app/Http/Controllers/Admin/DepositMethodController.php
+++ app/Http/Controllers/Admin/DepositMethodController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Storage;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use App\Models\DepositMethod;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
-use App\Models\DepositMethod;
-use Storage;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
 class DepositMethodController extends Controller
 {
@@ -21,14 +22,14 @@ class DepositMethodController extends Controller
     public function store(Request $request){
         $request->validate([
             'name' => 'required',
-            'image_url' => 'required',
+            'image' => 'required',
             'fixed_deposit_fee' => 'required',
             'percentage_deposit_fee' => 'required',
             'status' => 'required'
         ]);
 
         try {
-            $image = $request->file('image_url')->store('/DepositMethod/Image','public');
+            $image = $request->file('image')->store('/DepositMethod/Image','public');
             $image_url = Storage::disk('public')->url($image);
 
             DepositMethod::create([
@@ -47,5 +48,57 @@ class DepositMethodController extends Controller
         }
     }
 
-    
+    public function update(Request $request, DepositMethod $depositMethod){
+        $request->validate([
+            'name' => 'required',
+            'image' => 'required',
+            'fixed_deposit_fee' => 'required',
+            'percentage_deposit_fee' => 'required',
+            'status' => 'required'
+        ]);
+
+        try {
+
+            $data = [
+                'name' => $request->name,
+                'fixed_deposit_fee' => $request->fixed_deposit_fee,
+                'percentage_deposit_fee' => $request->percentage_deposit_fee,
+                'status' => $request->status
+            ];
+
+            if($request->hasFile('image')){
+                if(Storage::disk('public')->exists($depositMethod->image_name))
+                    Storage::disk('public')->delete($depositMethod->image_name);
+
+                $image = $request->file('image')->store('/DepositMethod/Image','public');
+                $image_url = Storage::disk('public')->url($image); 
+
+                $data['image_name'] = $image;
+                $data['image_url'] = $image_url;
+                
+            }
+
+            DepositMethod::where('id',$depositMethod->id)->update($data);
+
+            return back()->with(['success' => 'true', 'message' => 'Deposit Method Updated Successfully.']);
+            
+        } catch (\Throwable $th) {
+            Log::error('Deposit method Update Error : '. $th->getMessage());
+            return back()->with(['success' => 'false', 'message' => 'Operation Faield.']);
+        }
+    }
+
+    public function destroy(DepositMethod $depositMethod){
+
+        try {
+            if(Storage::disk('public')->exists($depositMethod->image_name))
+                Storage::disk('public')->delete($depositMethod->image_name);
+        } catch (FileNotFoundException $th) {
+            Log::error($th->getMessage());
+        }
+        
+        $depositMethod->delete();
+
+        return back()->with(['success' => 'true', 'message' => 'Deposit method deleted successfully.']);
+    }
 }
