073118a664715a404159ab9cb046d9683e3946e0
asif khan <asiff080@gmail.com>
2023-06-17 20:47:54 +0500

    setup users page

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UsersController extends Controller
 {
     public function index(){
-        return view('admin.users.index');
+        $users = User::wherehas('roles',function($query){
+            $query->whereNot('name','admin');
+        })->get();
+        return view('admin.users.index')->with('users',$users);
     }
 }
