8f9830342113f3374c4ce9022db3dc94d3cff054
asif khan <asiff080@gmail.com>
2023-06-22 09:29:01 +0500

    add index function in currecyController

--- app/Http/Controllers/Admin/CurrencyController.php
+++ app/Http/Controllers/Admin/CurrencyController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Currency;
 use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
-    //
+    public function index(){
+        $currencies = Currency::all();
+        return view('admin.currencies.index')->with('currencies',$currencies);
+    }
 }
